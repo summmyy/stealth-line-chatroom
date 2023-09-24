b693acb89026c55df090ee4e66da53ead52a4f91
Summy <Sunmibolaj13@gmail.com>
2023-09-23 21:59:20 -0400

    server commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
-
 import * as middleware from "./utils/middleware.js";
 import helloRoute from "./routes/helloRouter.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 // parse json request body
 app.use(express.json());
 
-// enable cors
+// enable CORS for Express routes
 app.use(cors());
 
 // request logger middleware
@@ -27,4 +30,26 @@ app.use("/hello", helloRoute);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-export default app;
+// Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  // Handle incoming messages from clients
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients (including the sender)
+    io.emit("message", message);
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected: ${socket.id}`);
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
+});
+
+export { app, server };
